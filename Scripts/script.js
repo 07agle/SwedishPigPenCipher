7ee9ec4b41d97cb5cd565c672f18210e509cecfc
Leon Agersten <leon.agersten@gmail.com>
2026-04-16 12:43:11 +0200

    Added weighted learning system in the letter mode

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,42 +6,40 @@ const skipButton = document.getElementById("skipButton");
 const timer = document.getElementById("timer"); 
 const timerSwitchButton = document.getElementById("timerSwitch"); 
 
-const letterList = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "x",
-  "y",
-  "z",
-  "å",
-  "ä",
-  "ö",
-];
+let letterMap = new Map([
+  ["a", 1], ["b", 1], ["c", 1],  ["d", 1], ["e", 1], ["f", 1],
+  ["g", 1], ["h", 1], ["i", 1],  ["j", 1], ["k", 1], ["l", 1],
+  ["m", 1], ["n", 1], ["o", 1],  ["p", 1], ["r", 1], ["s", 1], 
+  ["t", 1], ["u", 1],  ["v", 1], ["x", 1], ["y", 1], ["z", 1], 
+  ["å", 1], ["ä", 1],  ["ö", 1], 
+])
 
 let letter;
+let maxWeight = 10;
+let letterArray = [];
 let score = 0;
 let longestStreak = 0;
 let startTime = 5;
 let time = startTime;
 let timerOn = true;
 let intervalId;
+
+
+function generateArray(){
+  letterArray = [];
+
+  for(let [letter, weight] of letterMap){
+    for(let i = 0; i < weight; i++){
+      letterArray.push(letter);
+    }
+  }
+  console.log(letterArray);
+}
+
+function getRandomLetter(){
+  return letterArray[Math.floor(Math.random() * letterArray.length)];
+}
+
 reset();
 
 skipButton.onclick = reset;
@@ -56,12 +54,14 @@ userInput.addEventListener("keydown", function (event) {
 });
 
 function reset() {
-  let randomIndex = Math.floor(Math.random() * letterList.length);
+  clearInterval(intervalId);
+  generateArray();
+  let random = getRandomLetter();
 
   time = startTime;
   timer.textContent = "Timer: " + time;
 
-  letter = letterList[randomIndex];
+  letter = random;
   displayLetter.innerHTML = letter;
   userInput.value = "";
   document.body.style.backgroundColor = "aliceblue";
@@ -72,17 +72,21 @@ function guessLetter() {
   let guessedLetter = userInput.value.toLowerCase();
 
   if (guessedLetter === letter && time > 0) {
-    reset();
+    let current = letterMap.get(letter);
+    letterMap.set(letter, Math.max(1, current - 1));
     document.body.style.backgroundColor = "aliceblue";
     displayLetter.style.color = "#333";
     score++;
     scoreDisplay.innerHTML = "Score: " + score;
     if (score > longestStreak) {
-      longestStreak++;
-      streakDisplay.innerHTML = "Longest Streak: " + score;
+      longestStreak = score;
+      streakDisplay.innerHTML = "Longest Streak: " + longestStreak;
     }
+    reset();
   } else {
     score = 0;
+    let current = letterMap.get(letter);
+    letterMap.set(letter, Math.min(maxWeight, current + 1));
     document.body.style.backgroundColor = "#ff5757";
     displayLetter.style.color = "aliceblue";
     userInput.value = "";
@@ -111,7 +115,7 @@ function timerSwitch(){
     timerOn = false;
     timer.textContent = "";
   } else {
-    time = 5;
+    time = startTime;
     intervalId = setInterval(countdown, 1000);
     timerOn = true;
     timer.textContent = "Timer: " + time;
